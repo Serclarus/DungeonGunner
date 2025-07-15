91d7d7b9ffff4a77feee6bf503b4963fd5d10bb4
Serclarus <serefsakaci@hotmail.fr>
2025-07-15 16:01:58 +0300

    Player SO

--- Dungeon_Gunner/Assets/Scripts/Utilities/HelperUtilities.cs
+++ Dungeon_Gunner/Assets/Scripts/Utilities/HelperUtilities.cs
@@ -20,6 +20,15 @@ public class HelperUtilities
         return false;
     }
 
+    public static bool ValidateCheckNullValue(Object thisObject, string fieldName, UnityEngine.Object objectToCheck)
+    {
+        if (objectToCheck == null)
+        {
+            Debug.Log(fieldName + " is null and must contain a value in object " + thisObject.name.ToString());
+            return true;
+        }
+        return false;
+    }
 
     public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectToCheck)
     {
@@ -51,5 +60,27 @@ public class HelperUtilities
         }
         return error;
 
+    }
+
+    public static bool ValidateCheckPositiveValue(Object thisObject, string fieldName, int valueToCheck, bool isZeroAllowed)
+    {
+        bool error = false;
+        if (isZeroAllowed)
+        {
+            if (valueToCheck < 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value or zero in object " + thisObject.name.ToString());
+                error = true;
+            }
+        }
+        else
+        {
+            if (valueToCheck <= 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value in object " + thisObject.name.ToString());
+                error = true;
+            }
+        }
+        return error;
     }
 }
