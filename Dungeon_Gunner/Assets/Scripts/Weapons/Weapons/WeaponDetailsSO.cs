ce5420ec7e7e27d37f938d8e7fc195fcd9bd3521
Serclarus <serefsakaci98@gmail.com>
2025-12-28 23:50:09 +0300

    Pistol Ammo Prefab

--- Dungeon_Gunner/Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
+++ Dungeon_Gunner/Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
@@ -26,10 +26,10 @@ public class WeaponDetailsSO : ScriptableObject
     #endregion Tooltip
     public Vector3 weaponShootPosition;
 
-    //#region Tooltip
-    //[Tooltip("Weapon current ammo")]
-    //#endregion Tooltip
-    //public AmmoDetailsSO weaponCurrentAmmo;
+    #region Tooltip
+    [Tooltip("Weapon current ammo")]
+    #endregion Tooltip
+    public AmmoDetailsSO weaponCurrentAmmo;
 
     #region Header WEAPON OPERATING VALUES
     [Space(10)]
@@ -72,7 +72,7 @@ public class WeaponDetailsSO : ScriptableObject
     private void OnValidate()
     {
         HelperUtilities.ValidateCheckEmptyString(this, nameof(weaponName), weaponName);
-        //HelperUtilities.ValidateCheckNullValue(this, nameof(weaponCurrentAmmo), weaponCurrentAmmo);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(weaponCurrentAmmo), weaponCurrentAmmo);
         HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponFireRate), weaponFireRate, false);
         HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponPrechargeTime), weaponPrechargeTime, true);
 
